2c5287b2ee1f6053d7631a0bf97e6e051205e64e
Ilias Brinias <46651446+IliasBrinias@users.noreply.github.com>
2022-12-26 18:09:10 +0200

    Initial commit

---
+++ src/main/java/com/unipi/msc/jobfinderapi/JobFinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.unipi.msc.jobfinderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFinderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobFinderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unipi/msc/jobfinderapi/JobFinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipi.msc.jobfinderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobFinderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
